f82ac21864dce1ceae3364a4368bfb4fee615628
kcinnd <152316054+kcinnd@users.noreply.github.com>
2024-03-12 17:18:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,15 @@ let userInput = [];
 images.forEach(image => {
     image.addEventListener('click', () => {
         userInput.push(parseInt(image.dataset.sequence));
+        image.classList.add('clicked'); // Add the shading effect to clicked images
+
         if (userInput.length === sequence.length) {
             if (JSON.stringify(userInput) === JSON.stringify(sequence)) {
                 popupScreen.classList.remove('hidden');
                 popupScreen.classList.add('visible');
             } else {
                 userInput = []; // Reset if the sequence is incorrect
+                images.forEach(img => img.classList.remove('clicked')); // Remove the shading effect from all images
             }
         }
     });
@@ -22,4 +25,5 @@ popupScreen.addEventListener('click', () => {
     popupScreen.classList.add('hidden');
     popupScreen.classList.remove('visible');
     userInput = []; // Reset the sequence
+    images.forEach(img => img.classList.remove('clicked')); // Remove the shading effect from all images
 });
